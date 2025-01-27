5b0913ac858b4b64a5fbf7b485636502a492eab9
hungntt <minatosnow@gmail.com>
2025-01-26 20:29:24 -0400

    dclose fasterrcnn coco inference

---
+++ eval_dclose_fastercnn.py
@@ -0,0 +1,118 @@
+import torch
+import torchvision
+import os
+from data.coco.dataloader import coco_gt_loader
+import cv2
+import numpy as np
+from metrics.del_ins import del_ins
+from metrics.pp_ebpg import correspond_box, metric
+from torchvision import transforms as T
+from tqdm import tqdm
+from data.coco.dataloader import coco_gt_loader
+from xai_methods.dclose import DCLOSE
+from xai_methods.drise import DRISE
+from xai_methods.tool import (
+    get_prediction_fasterrcnn,
+    get_prediction_fasterrcnn_only_boxes,
+    visual,
+)
+from skimage.transform import resize
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(
+    pretrained=True, weights="DEFAULT"
+)
+model.eval().to(device)
+output_dir = "xai_methods/output/dclose/fasterrcnn"
+os.makedirs(output_dir, exist_ok=True)
+
+mean_del_auc = []
+mean_ins_auc = []
+mean_ebpg = []
+mean_pg = []
+
+# Load all images in the validation folder
+img_folder = "data/coco/val2017/"
+img_paths = [
+    os.path.join(img_folder, img_name)
+    for img_name in os.listdir(img_folder)
+    if img_name.endswith(".jpg")
+]
+
+info_data = coco_gt_loader()
+
+
+
+for img_path in tqdm(img_paths):
+    img = cv2.imread(img_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # (427, 640, 3)
+    transform = T.Compose([T.ToTensor()])
+    # preprocess image
+    inp = transform(img)
+    img_np = inp.numpy().transpose(1, 2, 0)
+    img_np = (255 * img_np).astype(np.uint8)
+    # file_name = img_path.split("/")[-1]
+    # img_name = file_name.split(".")[0]
+    # h, w, c = img.shape
+    # saliency_maps = np.array([])
+    with torch.no_grad():
+        prediction = model([inp.to(device)])
+        boxes = get_prediction_fasterrcnn_only_boxes(prediction, 0.8)
+        # boxes, pred_cls, pred = rs
+        # for i in range(len(boxes)):
+        # boxes[i].append(pred_cls[i])
+        # boxes[i].append(pred[i])
+        dclose = DCLOSE(
+            arch="faster-rcnn", model=model, img_size=(inp.shape[1:]), n_samples=100
+        )
+        # Should compute the saliency map for each box, not just the first one
+        # box = boxes[0]
+        rs = dclose(inp, boxes)
+        visual(img_np, rs, boxes, arch="faster_rcnn")
+        # np.save(f"{output_dir}/{img_name}.npy", saliency_maps)
+
+    # # Calculate metrics
+    # saliency_map = np.load(f"{output_dir}/{img_name}.npy")
+
+    # # Convert each box to the desired format and store in a list
+    # formatted_boxes = []
+    # for box in boxes:
+    #     x_min, y_min = box[0]
+    #     x_max, y_max = box[1]
+    #     class_id = np.float32(box[2])  # Convert integer to float32 for consistency
+    #     score = box[3]
+    #     # Construct the row with an additional calculated confidence value
+    #     formatted_boxes.append([x_min, y_min, x_max, y_max, score, class_id])
+
+    # gt_box, idx_correspond = correspond_box(formatted_boxes, info_data[file_name])
+    # if len(idx_correspond) == 0:
+    #     continue
+    # rgb_saliency_map = cv2.cvtColor(saliency_map, cv2.COLOR_BGR2RGB)
+    # resized_saliency_map = resize(rgb_saliency_map, (3, h, w), anti_aliasing=True)
+    # # Should extend to 3D array where first dim is the number of saliency maps
+    # # resized_saliency_map = np.expand_dims(resized_saliency_map, axis=0)
+    # ebpg, pg, count = metric(gt_box, resized_saliency_map[idx_correspond, :, :])
+    # mean_ebpg.append(np.mean(ebpg[count != 0] / count[count != 0]))
+    # mean_pg.append(np.mean(pg[count != 0] / count[count != 0]))
+    # del_auc, count = del_ins(
+    #     model=model,
+    #     img=img,
+    #     bbox=formatted_boxes,
+    #     saliency_map=resized_saliency_map,
+    #     arch="fasterrcnn",
+    #     mode="del",
+    #     step=2000,
+    # )
+    # ins_auc, count = del_ins(
+    #     model=model,
+    #     img=img,
+    #     bbox=formatted_boxes,
+    #     saliency_map=resized_saliency_map,
+    #     arch="fasterrcnn",
+    #     mode="ins",
+    #     step=2000,
+    # )
+    # mean_del_auc.append(np.mean(del_auc[count != 0] / count[count != 0]))
+    # mean_ins_auc.append(np.mean(ins_auc[count != 0] / count[count != 0]))
